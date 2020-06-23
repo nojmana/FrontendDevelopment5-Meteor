002892f9a82b27cc0065160d14e8236f79eb8b4b
nojmana <neumannkinga7@gmail.com>
2020-06-23 19:11:29 +0200

    Sorting with the possibility to change order

--- movies-ranking/imports/ui/App.js
+++ movies-ranking/imports/ui/App.js
@@ -6,6 +6,8 @@ import { Movies } from '../api/movies.js';
 
 import Movie from './Movie.js';
 
+const sort = new ReactiveVar(-1);
+
 // App component - represents the whole app
 class App extends Component {
     handleSubmit(event) {
@@ -22,38 +24,47 @@ class App extends Component {
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
+    changeSortingOrder() {
+        sort.set(sort.get() * (-1));
+    }
+
 
     renderMovies() {
+
         return this.props.movies.map((movie) => (
             <Movie key={movie._id} movie={movie} />
         ));
     }
 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h2>List of movies</h2>
+    render() {
+        return (
+        <div className="container">
+            <header>
+            <h2>List of movies</h2>
 
-          <form className="new-movie" onSubmit={this.handleSubmit.bind(this)} >
-            <input
-              type="title"
-              ref="textInput"
-              placeholder="Type to add a new movie"
-            />
-          </form>
-        </header>
+            <button className="sort" onClick = {this.changeSortingOrder.bind(this)} >
+                &#8645; </button>
 
-        <ul>
-          {this.renderMovies()}
-        </ul>
-      </div>
-    );
-  }
+            <form className="new-movie" onSubmit={this.handleSubmit.bind(this)} >
+                <input
+                type="title"
+                ref="textInput"
+                placeholder="Type to add a new movie"
+                />
+            </form>
+            </header>
+
+            <ul>
+                {this.renderMovies()}
+            </ul>
+        </div>
+        );
+    }
 }
 
 export default withTracker(() => {
+    console.log(sort.get())
     return { 
-        movies: Movies.find({}, { sort : { ranking : -1} }).fetch(),
+        movies: Movies.find({}, { sort : { ranking : sort.get() } }).fetch(),
     };
 })(App);
